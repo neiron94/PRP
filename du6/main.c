99709581628844a4f8887cbc69d39ecc48e108c8
Ales Shvaibovich <neironis94@gmail.com>
2022-11-22 21:47:50 +0100

    Working version

--- du6/main.c
+++ du6/main.c
@@ -30,6 +30,13 @@ int reduce(Matrix**, char**, int*, int*);
 void clean_matrices(Matrix**, int);
 void clean_matrix(Matrix*, int);
 void clean_operators(char**);
+int calculate(Matrix*, Matrix*, char*, int);
+void matrix_multiply(Matrix, Matrix, Matrix*);
+void matrix_add(Matrix*, Matrix);
+void matrix_subtraction(Matrix*, Matrix);
+void move_matrices(Matrix*, int, int);
+void move_operators(char*, int, int);
+void print_matrix(Matrix);
 
 
 int main(int argc, char *argv[])
@@ -44,11 +51,20 @@ int main(int argc, char *argv[])
 
   ret = read_input(&matrices, &operators, &mat_count, &oper_count);
 
-  //TODO
+  Matrix result;
+  result.values = NULL;
+  if (ret == EXIT_SUCCESS)
+    ret = calculate(&result, matrices, operators, oper_count);
+
+  if (ret == EXIT_SUCCESS)
+    print_matrix(result);
 
   // Cleaning memory (matrices and operators)
   if (matrices)
     clean_matrices(&matrices, mat_count);
+
+  if (result.values)
+    clean_matrix(&result, result.rows);
 
   if (operators)
     clean_operators(&operators);
@@ -103,10 +119,8 @@ int read_input(Matrix **mtrxs, char **opers, int *mat_count, int *oper_count) {
       (*mat_count)++;
 
       // Reading current operator (or exit the loop)
-      getchar();  // eats '\n' after the last row of matrix values
-
       char operator;
-      if (scanf("%c", &operator) == EOF)  // exit the loop, if EOF
+      if (scanf(" %c", &operator) == EOF)  // exit the loop, if EOF
         break;
 
       if (operator == ADD || operator == SUB || operator == MUL) {
@@ -204,7 +218,7 @@ int reduce(Matrix **mtrxs, char **opers, int *mat_count, int *oper_count) {
 void clean_matrices(Matrix **matrices, int count) {
   /* Cleans array of matrices and sets pointer to NULL */
 
-  if (!(*matrices))
+  if (!matrices || !(*matrices))
     return;
 
   for (int i = 0; i < count; i++)
@@ -218,11 +232,12 @@ void clean_matrices(Matrix **matrices, int count) {
 void clean_matrix(Matrix* matrix, int rows) {
   /* Cleans values of the matrix and sets pointer to NULL */
 
-  if (!matrix->values)
+  if (!matrix || !matrix->values)
     return;
 
-  for (int i = 0; i < rows; i++)
+  for (int i = 0; i < rows; i++) {
     free(matrix->values[i]);
+  }
   
   free(matrix->values);
   matrix->values = NULL;
@@ -232,9 +247,126 @@ void clean_matrix(Matrix* matrix, int rows) {
 void clean_operators(char **operators) {
   /* Cleans array of operators and sets pointer to NULL */
 
-  if (!(*operators))
+  if (!operators || !(*operators))
     return;
 
   free(*operators);
   *operators = NULL;
+}
+
+
+int calculate(Matrix *result, Matrix *mtrxs, char *opers, int oper_count) {
+  /* Goes through array of operators twice. Firstly does all
+     multiplications, then all other operations. */
+
+  int ret = EXIT_SUCCESS;
+
+  for (int i = 0; i < oper_count; i++) {
+    if (opers[i] == MUL) {
+
+      if (mtrxs[i].columns != mtrxs[i + 1].rows)
+        return ERROR_INPUT;
+      
+      Matrix tmp_matrix;
+      tmp_matrix.rows = mtrxs[i].rows;
+      tmp_matrix.columns = mtrxs[i + 1].columns;
+      ret = allocate_values(&tmp_matrix);
+      if (ret != EXIT_SUCCESS)
+        return ret;
+      matrix_multiply(mtrxs[i], mtrxs[i + 1], &tmp_matrix);
+
+      clean_matrix(&mtrxs[i], mtrxs[i].rows);
+      clean_matrix(&mtrxs[i + 1], mtrxs[i + 1].rows);
+
+      move_matrices(mtrxs, i + 1, oper_count);
+      mtrxs[i] = tmp_matrix;
+      move_operators(opers, i + 1, oper_count - 1);
+
+      oper_count--;
+      i--;
+    }
+  }
+
+  result->rows = mtrxs[0].rows;
+  result->columns = mtrxs[0].columns;
+  ret = allocate_values(result);
+  if (ret != EXIT_SUCCESS)
+    return ret;
+  for (int i = 0; i < result->rows; i++)
+    for (int j = 0; j < result->columns; j++)
+      result->values[i][j] = mtrxs[0].values[i][j];
+
+  for (int i = 0; i < oper_count; i++) {
+    if (result->rows != mtrxs[i + 1].rows || result->columns != mtrxs[i + 1].columns)
+      return ERROR_INPUT;
+
+    if (opers[i] == ADD)
+      matrix_add(result, mtrxs[i + 1]);
+    else 
+      matrix_subtraction(result, mtrxs[i + 1]);
+  }
+
+  return EXIT_SUCCESS;
+}
+
+
+void matrix_multiply(Matrix left, Matrix right, Matrix *result) {
+
+  for (int i = 0; i < result->rows; i++) {
+    for (int j = 0; j < result->columns; j++) {
+      result->values[i][j] = 0;
+      for (int n = 0; n < left.columns; n++) {
+        result->values[i][j] += left.values[i][n] * right.values[n][j];
+      }
+    }
+  }
+}
+
+
+void matrix_add(Matrix *left, Matrix right) {
+
+  for (int i = 0; i < left->rows; i++)
+    for (int j = 0; j < left->columns; j++)
+      left->values[i][j] += right.values[i][j];
+}
+
+
+void matrix_subtraction(Matrix *left, Matrix right) {
+
+  for (int i = 0; i < left->rows; i++)
+    for (int j = 0; j < left->columns; j++)
+      left->values[i][j] -= right.values[i][j];
+}
+
+
+void move_matrices(Matrix *mtrxs, int start_i, int end_i) {
+
+  for (int i = start_i; i <= end_i; i++) {
+    mtrxs[i - 1] = mtrxs[i];
+    mtrxs[i].values = NULL;
+  }
+}
+
+
+void move_operators(char *opers, int start_i, int end_i) {
+
+  for (int i = start_i; i <= end_i; i++)
+    opers[i - 1] = opers[i];
+}
+
+
+void print_matrix(Matrix matrix) {
+  /* Prints one matrix */
+
+  printf("%d %d\n", matrix.rows, matrix.columns);
+
+  for (int i = 0; i < matrix.rows; i++) {
+    for (int j = 0; j < matrix.columns; j++) {
+      if (j != 0)
+        printf(" ");
+      
+      printf("%d", matrix.values[i][j]);
+    }
+    printf("\n");
+  }
 }
